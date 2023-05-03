a0286b14658f6ffeef82d0eada9868740e967440
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-02 20:58:27 -0700

    modified login logic to accommodate registration and login

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
             });
 
             if (response.ok) {
-                document.location.replace('/views/layouts/dashboard.handlebars');
+                document.location.replace('/dashboard');
             } else {
                 alert('Unable to log you in at this time. Please verify your email and password and try again.');
             }
@@ -23,4 +23,28 @@ const loginFormHandler = async (event) => {
     }
 };
 
+const signupFormHandler = async (event) => {
+event.preventDefault();
+
+const email = document.querySelector('#email-signup').value.trim();
+const userName = email.split('@')[0];
+const password = document.querySelector('#password-signup').value.trim();
+
+if (userName && email && password) {
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ userName, email, password }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('You forgot to say the magic word.');
+  }
+}
+};
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
