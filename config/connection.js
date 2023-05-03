9c511a0939f4347f3c9903764804d32f54007985
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-05-02 20:23:10 -0700

    connection.js

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3030
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Steppin\' in the right direction.');
+}).catch((err) => {
+    console.error('Steppin\' in the wrong direction:', err);
+});
+
+module.exports = sequelize;
